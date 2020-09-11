0470049a9d9b37f2c6fc969ee49821fd402ffd1d
RNtaate <royhyde14@gmail.com>
2020-09-11 20:52:47 +0300

    Update index method in tweeets_controller

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -5,6 +5,7 @@ class TweeetsController < ApplicationController
   # GET /tweeets.json
   def index
     @tweeets = Tweeet.all.order("created_at DESC")
+    @tweeet = Tweeet.new
   end
 
   # GET /tweeets/1
