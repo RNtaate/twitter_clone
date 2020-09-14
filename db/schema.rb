873064016112f3b0a810eeaac0f23c7cc386b371
RNtaate <royhyde14@gmail.com>
2020-09-14 17:34:21 +0300

    Create AddFieldsToUser migration to add more columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_140721) do
+ActiveRecord::Schema.define(version: 2020_09_14_142831) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 2020_09_14_140721) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
